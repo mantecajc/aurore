64ae8d64d1b722bb7080f75e8c1f4e53c27ed077
Javier <65590523+javierc435@users.noreply.github.com>
2020-11-03 13:47:17 +0100

    Init edit and new form

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,20 +1,39 @@
 class SongsController < ApplicationController
   def index
-
+    @songs = Song.all
   end
 
   def new
+    @song = Song.new
   end
 
   def create
-    @song = Song.new(article_params)
+    @song = Song.new(song_params)
 
     @song.save
     redirect_to @song
   end
 
+  def show
+    @song = Song.find(params[:id])
+  end
+
+  def edit
+    @song = Song.find(params[:id])
+  end
+
+  def update
+    @song = Song.find(params[:id])
+
+    if @song.update(song_params)
+      redirect_to @song
+    else
+      render 'edit'
+    end
+  end
+
   private
-    def article_params
+    def song_params
       params.require(:song).permit(:title, :description)
     end
 end
