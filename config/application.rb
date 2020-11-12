f6ee868209264c3130af2af174c95df2824e26df
Javier <65590523+javierc435@users.noreply.github.com>
2020-11-12 21:03:49 +0100

    Fixed application.rb

--- config/application.rb
+++ config/application.rb
@@ -7,13 +7,14 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-    config.assets.initialize_on_precompile = false
-    
+
+
 module Aurore
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
     config.assets.paths << Rails.root.join("app", "assets", "img")
+    config.assets.initialize_on_precompile = false
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
