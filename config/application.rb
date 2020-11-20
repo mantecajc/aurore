48f548f05dbd1a8660ed7ae5b1a83a113d77ad18
Javier <65590523+javierc435@users.noreply.github.com>
2020-11-20 08:22:29 +0100

    Update application.rb for Heroku

--- config/application.rb
+++ config/application.rb
@@ -14,7 +14,8 @@ module Aurore
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
     config.assets.paths << Rails.root.join("app", "assets", "img")
-
+    # Needed for Heroku
+    config.assets.initialize_on_precompile = false 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
