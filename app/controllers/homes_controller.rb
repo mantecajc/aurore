02a12b29861a3df6e302bb487de0e2823a579da5
Javier <65590523+javierc435@users.noreply.github.com>
2020-12-12 15:59:43 +0100

    Init editable home page

---
+++ app/controllers/homes_controller.rb
@@ -0,0 +1,39 @@
+class HomesController < ApplicationController
+  def index
+    @homes = Home.all.order("created_at DESC")
+  end
+
+  def new
+    @home = Home.new
+  end
+
+  def create
+    @home = Home.new(home_params)
+
+    @home.save
+    redirect_to @home
+  end
+
+  def show
+    @home = Home.find(params[:id])
+  end
+
+  def edit
+    @home = Home.find(params[:id])
+  end
+
+  def update
+    @home = Home.find(params[:id])
+
+    if @home.update(home_params)
+      redirect_to @home
+    else
+      render 'edit'
+    end
+  end
+
+  private
+    def home_params
+      params.require(:home).permit(:main_description, :sub_description)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'home#index'
+  root 'homes#index'
+  resources :homes
 
   resources :songs
  
