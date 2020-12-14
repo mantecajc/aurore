3d2e85f8ffc9b807bb10b937221678f738650559
Javier <65590523+javierc435@users.noreply.github.com>
2020-12-14 22:02:12 +0100

    Disable authentication for home page edition

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,5 +1,5 @@
 class HomesController < ApplicationController
-  before_action :authenticate_user!, except: [:index]
+  #before_action :authenticate_user!, except: [:index]
   
   def index
     @homes = Home.all.order("created_at DESC")
